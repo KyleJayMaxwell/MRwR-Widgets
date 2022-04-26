b9e271b23000267ab73300e1924abebd6c4d3b70
Kyle Maxwell <kylejaymaxwell@gmail.com>
2022-04-26 13:40:15 -0700

    [newBranch] reseting some other commits

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search);
+        };
+        search();
+    }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return <div className="item">
+            <div className="content">
+                <div className="header">
+                    
+                </div>
+            </div>
+        </div>
+    })
     return (
         <div>
             <div className="ui form">
